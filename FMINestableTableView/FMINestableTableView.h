4f7a551e92fd178ee8c4d67167d3a2c5d2809f1b
Florian Mielke <Florian@BiffyBook.Speedport_W723_V_Typ_A_1_01_016>
2016-06-15 11:20:12 +0200

    Add initial project files

---
+++ FMINestableTableView/FMINestableTableView.h
@@ -0,0 +1,19 @@
+//
+//  FMINestableTableView.h
+//  FMINestableTableView
+//
+//  Created by Florian Mielke on 15.06.16.
+//  Copyright © 2016 Florian Mielke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMINestableTableView.
+FOUNDATION_EXPORT double FMINestableTableViewVersionNumber;
+
+//! Project version string for FMINestableTableView.
+FOUNDATION_EXPORT const unsigned char FMINestableTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMINestableTableView/PublicHeader.h>
+
+
