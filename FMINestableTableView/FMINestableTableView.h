706ecf8effb1a20f3e279d4bc322eded7eb475f8
Florian Mielke <Florian@BiffyBook.Speedport_W723_V_Typ_A_1_01_016>
2016-06-16 15:48:00 +0200

    Enhance header documentation

--- FMINestableTableView/FMINestableTableView.h
+++ FMINestableTableView/FMINestableTableView.h
@@ -10,8 +10,107 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol FMINestableTableViewDelegate;
-@protocol FMINestableTableViewDataSource;
+@class FMINestableTableView;
+
+
+/**
+ * The FMINestableTableViewDataSource protocol is adopted by an object that mediates the application’s data model for a FMINestableTableView object.
+ * The data source provides the table-view object with the information it needs to construct and modify a table view.
+ */
+@protocol FMINestableTableViewDataSource <NSObject, UITableViewDataSource>
+
+/**
+ * Asks the data source to return the number of nested rows for a given index path.
+ * @param tableView The table-view object requesting this information.
+ * @param indexPath An index path locating a row in tableView.
+ * @return The number of nested rows for indexPath.
+ */
+- (NSInteger)nestableTableView:(FMINestableTableView *)tableView numberOfNestedRowsForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *
+ * @param tableView The table-view object requesting this information.
+ * @param indexPath An index path locating a row in tableView.
+ * @return YES if the row indicated by indexPath has nested rows; otherwise, NO.
+ */
+- (BOOL)nestableTableView:(FMINestableTableView *)tableView hasNestedRowsForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ * Asks the data source for a cell to insert in a particular location of the table view.
+ * @param tableView The table-view object requesting this information.
+ * @param indexPath An index path locating a row in tableView.
+ * @return An object inheriting from UITableViewCell that the table view can use for the specified row. An assertion is raised if you return nil.
+ */
+- (UITableViewCell *)nestableTableView:(FMINestableTableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *
+ * @param tableView The table-view object requesting this information.
+ * @param index
+ * @param indexPath
+ * @return
+ */
+- (UITableViewCell *)nestableTableView:(FMINestableTableView *)tableView cellForNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
+
+@optional
+/**
+ *
+ * @param tableView The table-view object requesting this information.
+ * @param indexPath
+ * @return
+ */
+- (BOOL)nestableTableView:(FMINestableTableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *
+ * @param tableView
+ * @param index
+ * @param indexPath
+ * @return
+ */
+- (BOOL)nestableTableView:(FMINestableTableView *)tableView canEditNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *
+ * @param tableView The table-view object requesting this information.
+ * @param editingStyle
+ * @param indexPath
+ */
+- (void)nestableTableView:(FMINestableTableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *
+ * @param tableView The table-view object requesting this information.
+ * @param editingStyle
+ * @param indexPath
+ * @param index
+ * @param rootIndexPath
+ */
+- (void)nestableTableView:(FMINestableTableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forNestedRowAtIndexPath:(NSIndexPath *)indexPath nestedItemIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)rootIndexPath;
+
+@end
+
+/**
+ * The delegate of a \c FMINestableTableView object must adopt the FMINestableTableViewDelegate protocol.
+ */
+@protocol FMINestableTableViewDelegate <NSObject, UITableViewDelegate>
+
+/**
+ * Tells the delegate that the specified row is now selected.
+ * @param tableView The table-view object requesting this information.
+ * @param indexPath An index path locating the new selected row in tableView.
+ */
+- (void)nestableTableView:(FMINestableTableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ * Tells the delegate that the specified nested row is now selected.
+ * @param tableView The table-view object requesting this information.
+ * @param index An index locating the new selected nested row in tableView.
+ * @param indexPath An index path that locates a root row in tableView.
+ */
+- (void)nestableTableView:(FMINestableTableView *)tableView didSelectNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
+
+@end
 
 /**
  *
@@ -19,22 +118,22 @@ NS_ASSUME_NONNULL_BEGIN
 @interface FMINestableTableView : UITableView
 
 /**
- *
+ * The object that acts as the data source of the table view.
  */
 @property (nullable, weak, NS_NONATOMIC_IOSONLY) id <FMINestableTableViewDataSource> dataSource;
 
 /**
- *
+ * The object that acts as the delegate of the table view.
  */
 @property (nullable, weak, NS_NONATOMIC_IOSONLY) id <FMINestableTableViewDelegate> delegate;
 
 /**
- *
+ * A Boolean value that determines whether the table view uses nested rows.
  */
 @property (NS_NONATOMIC_IOSONLY) IBInspectable BOOL allowsNestedRows;
 
 /**
- *
+ * A Boolean value that indicates whether the tabe view shows nested rows.
  */
 @property (readonly, NS_NONATOMIC_IOSONLY) BOOL showsNestedRows;
 
@@ -90,104 +189,6 @@ NS_ASSUME_NONNULL_BEGIN
  *
  */
 - (void)hideNestedRows;
-
-@end
-
-/**
- *
- */
-@protocol FMINestableTableViewDataSource <UITableViewDataSource>
-
-/**
- *
- * @param tableView
- * @param indexPath
- * @return
- */
-- (NSInteger)nestableTableView:(FMINestableTableView *)tableView numberOfNestedRowsForRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param indexPath
- * @return
- */
-- (BOOL)nestableTableView:(FMINestableTableView *)tableView hasNestedRowsForRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param indexPath
- * @return
- */
-- (UITableViewCell *)nestableTableView:(FMINestableTableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param index
- * @param indexPath
- * @return
- */
-- (UITableViewCell *)nestableTableView:(FMINestableTableView *)tableView cellForNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
-
-@optional
-/**
- *
- * @param tableView
- * @param indexPath
- * @return
- */
-- (BOOL)nestableTableView:(FMINestableTableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param index
- * @param indexPath
- * @return
- */
-- (BOOL)nestableTableView:(FMINestableTableView *)tableView canEditNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param editingStyle
- * @param indexPath
- */
-- (void)nestableTableView:(FMINestableTableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param editingStyle
- * @param indexPath
- * @param index
- * @param rootIndexPath
- */
-- (void)nestableTableView:(FMINestableTableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forNestedRowAtIndexPath:(NSIndexPath *)indexPath nestedItemIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)rootIndexPath;
-
-@end
-
-/**
- *
- */
-@protocol FMINestableTableViewDelegate <UITableViewDelegate>
-
-/**
- *
- * @param tableView
- * @param indexPath
- */
-- (void)nestableTableView:(FMINestableTableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *
- * @param tableView
- * @param index
- * @param indexPath
- */
-- (void)nestableTableView:(FMINestableTableView *)tableView didSelectNestedRowAtIndex:(NSUInteger)index rootRowIndexPath:(NSIndexPath *)indexPath;
 
 @end
 
